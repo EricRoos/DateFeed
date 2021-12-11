69483a7d65362ac286870779cb90a70d974e0ff6
Eric Roos <ericroos@hey.com>
2021-12-11 13:23:14 -0600

    add spec

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,12 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :profile, ProfileType, "Find a profile by id" do
+      argument :id, ID
+    end
+
+    def profile(id:)
+      Profile.find(id)
     end
   end
 end
---
+++ spec/requests/fetch_profiles_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "FetchProfiles", type: :request do
+  describe "GRAPHQL #profile" do
+    let(:profile) { FactoryBot.create(:profile) }
+    let(:request) { <<-GQL
+      {
+        profile(id: #{profile.id}) {
+          name
+          age
+        }
+      }
+      GQL
+    }
+    let(:expected_response) do
+      {
+        data: {
+          profile: {
+            name: profile.name,
+            age: profile.age
+          }
+        }
+      }
+    end
+    before do
+      post '/graphql', params: { query: request }
+    end
+    subject { response }
+    it { is_expected.to have_attributes(body: expected_response.to_json) }
+    it { is_expected.to have_http_status(200) }
+  end
+end
