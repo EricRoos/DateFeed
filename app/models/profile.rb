5d8b4e94f68b60545a61edfe6bf379c358a584f9
Eric Roos <ericroos@hey.com>
2021-12-12 22:45:14 -0600

    shared_interactions_with

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,20 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  def shared_interactions_with
+    interaction_profiles = Hash.new(0)
+    PostInteraction
+      .joins(:post)
+      .joins(:profile)
+      .where("profiles.id = ? OR posts.profile_id = ?", id, id)
+      .group("profiles.id, posts.profile_id")
+      .order("profiles.id")
+      .pluck("profiles.id, posts.profile_id")
+      .flatten
+      .each { |profile_id| interaction_profiles[profile_id] += 1 unless profile_id == self.id}
+
+    interaction_profiles
+      .select { |k,v| v > 1 }
+      .keys
+  end
 end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -5,4 +5,23 @@ RSpec.describe Profile, type: :model do
     subject { FactoryBot.build(:profile) }
     it { is_expected.to be_valid }
   end
+
+  describe '#shared_interactions_with' do
+    let(:other) { FactoryBot.create(:profile) }
+    let(:other2) { FactoryBot.create(:profile) }
+    let(:other3) { FactoryBot.create(:profile) }
+    let(:me) { FactoryBot.create(:profile) }
+
+    let!(:my_post) { FactoryBot.create(:post, profile: me, interacted_with_by: [other]) }
+    let!(:my_other_post) { FactoryBot.create(:post, profile: me, interacted_with_by: [other2]) }
+    let!(:my_other_other_post) { FactoryBot.create(:post, profile: me, interacted_with_by: [other3]) }
+
+    let!(:other_post) { FactoryBot.create(:post, profile: other, interacted_with_by: [me]) }
+    let!(:other_other_post) { FactoryBot.create(:post, profile: other3, interacted_with_by: [me]) }
+    let!(:misc_post) { FactoryBot.create(:post, interacted_with_by: [other]) }
+
+    subject { me.shared_interactions_with }
+
+    it { is_expected.to eq [other.id, other3.id] }
+  end
 end
