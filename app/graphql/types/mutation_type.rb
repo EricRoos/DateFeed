2f63d183be93612b6572acc7cacd707591309afc
Eric Roos <ericroos@hey.com>
2021-12-18 22:32:36 -0600

    add create post mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@
 
 module Types
   class MutationType < Types::BaseObject
+    field :create_post, mutation: Mutations::CreatePost
     field :toggle_post_interaction, mutation: Mutations::TogglePostInteraction
   end
 end
