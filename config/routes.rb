2d1426d953a44b0f4812cc37d72f31d39aae4714
Eric Roos <ericroos@hey.com>
2021-12-21 12:48:57 -0600

    remove graphiql from routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
   post '/graphql', to: 'graphql#execute'
   root to: 'dashboard#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
