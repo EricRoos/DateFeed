63002903c3defe7985d527ebcfc401f5a7d19800
Eric Roos <ericroos@hey.com>
2021-12-21 17:20:39 -0600

    more spec

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 RSpec.feature "Logins", type: :feature do
-  scenario 'add feature spec' do
+  scenario 'add feature spec', js: true do
     FactoryBot.create(:app_token, app_name: 'rails_app')
+    profile = FactoryBot.create(:profile)
     visit '/users/sign_in'
+    fill_in 'Email', with: profile.user.email
+    fill_in 'Password', with: 'test123456'
+    click_on 'Log in'
+    expect(page).to have_content('Activity Feed')
   end
 
 end
