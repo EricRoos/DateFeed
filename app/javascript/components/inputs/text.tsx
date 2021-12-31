46c200d603f5951f55f17571761388439274a8b8
Eric Roos <ericroos@hey.com>
2021-12-31 12:22:55 -0600

    good error states in forms

--- app/javascript/components/inputs/text.tsx
+++ app/javascript/components/inputs/text.tsx
@@ -9,18 +9,40 @@ interface OtherSearchFieldProps {
   label : string
 }
 const TextInput = ({ label, ...props} : OtherSearchFieldProps & FieldHookConfig<string>) => {
-   const [field, meta, helpers] = useField(props);
-   return (
-     <div className=''>
-       <label className='font-semibold block mb-1 text-md'>
-         {label}
-       </label>
-       <input {...field} disabled={props.disabled} type={props.type} className='text-black w-full border border-solid border-gray-300 bg-gray-100 px-4 py-2 rounded-lg focus:outline-none'/>
-       {meta.touched && meta.error ? (
-         <div className="error">{meta.error}</div>
-       ) : null}
-     </div>
-   );
+  const [field, meta, helpers] = useField(props);
+  const fieldElement = React.createRef<HTMLInputElement>();
+
+  function MarkInvalid(){
+    const { current } = fieldElement;
+    if(current){
+      current.setCustomValidity(meta.error || "");
+    }
+  }
+
+  React.useEffect( () => {
+    MarkInvalid();
+  }, [field]);
+
+  return (
+    <div className=''>
+      <label className='font-semibold block mb-1 text-md'>
+        {label}
+      </label>
+      <input {...field}
+        onChange={ (ev) => {
+          helpers.setValue(ev.target.value);
+          MarkInvalid();
+        }}
+        ref={fieldElement}
+        disabled={props.disabled}
+        type={props.type}
+        className='text-black w-full border border-solid border-gray-300 bg-gray-100 px-4 py-2 rounded-lg focus:outline-none invalid:border-red-500'
+      />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </div>
+  );
 };
 
 export default TextInput;
